6cd37c6cd6a7e51f9a6d527247a6089c35f4d2e2
idilia wheat <lillyw54@gmail.com>
2023-11-22 00:56:55 -0600

    project

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('./config/connection');
+// import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}!`);
-    });
-  })
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
