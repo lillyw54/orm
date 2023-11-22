a69efc9f0f2548af20ba64bc7755fdfb0b98f759
idilia wheat <lillyw54@gmail.com>
2023-11-21 19:57:48 -0600

    progress

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+// import sequelize connection
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+// sync sequelize models to the database, then turn on the server
 sequelize
   .sync({ force: false })
   .then(() => {
@@ -17,6 +19,3 @@ sequelize
       console.log(`App listening on port ${PORT}!`);
     });
   })
-  .catch((err) => {
-    console.log('Unable to connect to the database:', err);
-  });
